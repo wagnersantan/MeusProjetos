04947570028b4f5ef1239f804cc19ac7b64a1c58
Wagner do Amparo Santana <wagneramparo@gmail.com>
2025-02-23 23:23:27 -0300

    modularização

---
+++ jogo.py
@@ -0,0 +1,22 @@
+# jogo.py
+
+import forca
+import advinhacao
+
+print("*********************************")
+print("****Escolha o seu jogo***********!")
+print("*********************************")
+
+print("(1) Forca (2) Adivinhação")
+
+jogo = int(input("Qual jogo? "))
+
+if (jogo == 1):
+    print("Jogando Forca")
+    forca.jogar()
+
+elif (jogo == 2):
+    print("Jogando Adivinhação")
+    advinhacao.jogar()
+  
+
